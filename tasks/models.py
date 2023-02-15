1c63500db4a59426902c91f306471a4292284f91
tmampa <tmampa94@gmail.com>
2023-02-15 13:51:24 +0200

    Create model, display the index page with lists

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields import CharField
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-created"]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    tasks = Task.objects.all()
+    context = {"tasks": tasks}
+    return render(request, "index.html", context)
